80f93fd2b0b217209145ca2c8c4dd19a1d2adea9
radoslav77 <rado7711@yahoo.co.uk>
2021-12-10 20:50:19 +0000

    0002

--- cost/urls.py
+++ cost/urls.py
@@ -6,4 +6,5 @@ from . import views
 # path to your urls
 urlpatterns = [
     path('', views.index, name='index'),
+    path('some_view_api', views.some_view_api, name='some_view_api')
 ]
--- cost/views.py
+++ cost/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+import csv
+from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
     return render(request, 'cost/index.html')
+
+
+def some_view_api(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+    writer.writerow(['Second row', 'A', 'B', 'C',
+                    '"Testing"', "Here's a quote"])
+    print(response)
+    return response
