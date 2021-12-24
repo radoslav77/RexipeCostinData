1f012288bb1401189b689b1ef302f040c1cb81a5
radoslav77 <rado7711@yahoo.co.uk>
2021-12-24 10:32:26 +0000

    #12

--- cost/views.py
+++ cost/views.py
@@ -2,39 +2,69 @@ from django.shortcuts import render
 import csv
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from pathlib import Path
+import pandas as pd
+from csv import DictReader
+import sys
+import pprint
+import os
+from django.conf import settings
 
 from .models import Exeldocument
 from django.views.generic.edit import FormView
 from .forms import FileFieldForm
+
 # Create your views here.
 
 
+dir_name = "/media/"
+
+
 def index(request):
+
     files = Exeldocument.objects.filter(title='ritter')
     csv_files = []
+    field_names = []
+    #new_path = settings.MEDIA_ROOT + file
 
-    for file in files:
-        csv_files.append(file)
-    # print(csv_files)
-    for ex in csv_files:
-        excel_sheet = ex.doc
-        print(ex.doc)
-        path = Path(ex.doc)
-        with open(path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                print(row['first_name'], row['last_name'])
-        for d in excel_sheet:
-            # print(d)
-            '''
-            with open(d, newline='') as csvfile:
-                spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                for row in spamreader:
-                    print(row['Description'], row['Unit Size'])
-                  # print(', '.join(row))
-                  '''
-    return render(request, 'cost/index.html')
+    # print(new_path)
+    def csv_dict_list(variables_file):
+        new_try = csv.DictReader(open(
+            'C:/Users/home/Desktop/django projects/Costing/RecipeCosting/media/media/SupplierPriceListExport_1009451_12-23-202117-09-42.csv', newline=''))
+        dict_list = []
+        for line in new_try:
+            dict_list.append(line)
+        return dict_list
+
+    device_values = csv_dict_list(sys.argv[1])
+    pprint.pprint(device_values)
+    '''
+        spamreader = csv.reader(f, delimiter=' ', quotechar='|')
+        reader = csv.DictReader(f)
+        order_dict_list = list(reader)[0]
+        dict_csv = dict(order_dict_list)
+        csv_files.append(dict_csv)
+        print(dict_csv)
+        # for row in spamreader:
+        # csv_files.append(row)
+        #dict_from_csv = {rows[0]: rows[1] for rows in spamreader}
+        # print(dict_from_csv)
+        '''
+    # open file in read mode
+    with open('C:/Users/home/Desktop/django projects/Costing/RecipeCosting/media/media/SupplierPriceListExport_1009451_12-23-202117-09-42.csv', 'r') as read_obj:
+        # pass the file object to DictReader() to get the DictReader object
+        csv_dict_reader = DictReader(read_obj)
+    # get column names from a csv file
+        column_names = csv_dict_reader.fieldnames
+        # field_names.append(column_names)
+        # print(column_names)
+
+    # for line in csv_files:
+        # print(line)
+
+    return render(request, 'cost/index.html', {
+        'csv': files,
+        'rows': device_values,
+    })
 
 
 def some_view_api(request):
